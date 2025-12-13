3b3f942e4c64afcf5b7e7f0c811c8bc9307f4d77
Marco Silva <marcosilvam159@gmail.com>
2025-12-13 18:31:51 -0300

    Repository and Services created for Movies

--- movies/page.py
+++ movies/page.py
@@ -1,25 +1,67 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 from st_aggrid import AgGrid
+from movies.services import MovieService
+from genres.service import GenreService
+from actors.service import ActorService
 
-
-movies = [
-    {
-        'id':1,
-        'name':'Rock N Hola'
-    }
-]
 
 def show_movies():
-    st.write("Lista de Filmes")
-
-    AgGrid(
-        data=pd.DataFrame(movies),
-        key='movies_grid',
-        reload=True
+    movie_service = MovieService()
+    movies = movie_service.get_movies()
+    if movies:    
+        st.write("Lista de Filmes")
+        movies_df = pd.json_normalize(movies)
+        AgGrid(
+            data=movies_df,
+            key='movies_grid',
+            reload=True
+        )
+    else:
+        st.warning('Nenhum Filme encontrado!')
+        
+        
+    st.title("Cadastrar novo Filme")
+    
+    title = st.text_input('Nome do Filme')
+    
+    genres_service = GenreService()
+    genres=genres_service.get_genres()
+    genre_names = {genre['name']: genre['id'] for genre in genres}
+    selected_genre_name = st.selectbox(
+        label='Gênero',
+        options = list(genre_names.keys()),
     )
     
-    st.title("Cadastrar novo Filme")
-    name = st.text_input('Nome do Filme')
+    actor_service = ActorService()
+    actors = actor_service.get_actors()
+    actor_names = {actor['name']: actor['id'] for actor in actors}
+    selected_actors_names = st.multiselect("Atores/Atrizes", list(actor_names.keys()))
+    selected_actors_ids = [actor_names[name] for name in selected_actors_names]
+    
+    release_date = st.date_input(
+        label='Data de lançamento',
+        value=datetime.today(),
+        min_value=datetime(1800,1,1).date(),
+        max_value=datetime.today(),
+        format='DD/MM/YYYY'
+   )
+    
+    resume = st.text_area('Resumo')
+    
+    
     if st.button('Cadastrar'):
-        st.success(f"Filme '{name}' cadastrado com sucesso!")
+        new_movie=movie_service.create_movie(
+            title=title,
+            genre=genre_names[selected_genre_name],
+            actors=selected_actors_ids,
+            release_date=release_date,
+            resume=resume,
+        )
+        if new_movie:
+            st.success(f"Filme '{title}' cadastrado com sucesso!")
+            st.rerun()
+        else:
+            st.error("Erro ao cadastrar o filme. Verifique os campos!")
+        
---
+++ movies/services.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+from movies.repository import MovieRepository
+
+
+class MovieService:
+    
+    
+    def __init__(self):
+        self.movie_repository = MovieRepository()
+    
+    
+    def get_movies(self):
+        return self.movie_repository.get_movies()
+    
+    
+    def create_movie(self, title, genre, actors, release_date, resume):
+        movie = dict(
+            title=title,
+            genre=genre,
+            actors=actors,
+            release_date=release_date,
+            resume=resume,
+        )
+        return self.movie_repository.create_movie(movie)
