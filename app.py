8003c286b85582dd009b6ff11b3fc9ca8f0dc536
Marco Silva <marcosilvam159@gmail.com>
2025-12-13 19:04:26 -0300

    created get_movie_stats function on movies/service, new directory home/ for building the homepage

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from genres.page import show_genres
 from actors.page import show_actors
 from movies.page import show_movies
 from reviews.page import show_reviews
+from home.page import show_home
 
 
 def  main():
@@ -20,7 +21,7 @@ def  main():
         )
         
         if menu_option == 'Início':
-            st.write('Início')
+            show_home()
             
         if menu_option == 'Gêneros':
             st.write('Gêneros')
---
+++ home/page.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+
+def show_home():
+    st.title("Estatísticas de Filmes")
